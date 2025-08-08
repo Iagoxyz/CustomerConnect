004e6080ce94d169b294571c569368458ed025c0
Iago Carvalho <iagocarvalhos10@gmail.com>
2025-08-08 10:07:02 -0300

    Project created

---
+++ src/test/java/tech/build/customerconnect/CustomerconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.build.customerconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
